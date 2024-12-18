7db7f4732e63a19fb65391ec54ce829f1bff3179
poriad_d <poriad_d@etna-alternance.net>
2024-12-18 18:25:42 +0000

    [ORGANIZATION]  -  file organization

--- Frontend/managementwebapp-react/src/components/createaccount.js
+++ Frontend/managementwebapp-react/src/components/createaccount.js
@@ -3,7 +3,7 @@ import '../style/login.css';
 function CreateAccount() {
   return (
     <div className="createaccount">
-      <p>New to GitHub? <a>Create an account</a></p>
+      <p>New? <a>Create an account</a></p>
     </div>
   );
 }
